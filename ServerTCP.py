86f65d53e03ade67b431c2acd03fdaa715d2fe10
makinay <vitor_souza97@hotmail.com>
2021-04-23 19:01:58 -0300

    Client basico concluido

--- ServerTCP.py
+++ ServerTCP.py
@@ -2,7 +2,7 @@ import socket
 import threading
 
 bind_ip = 'localhost'
-bind_port = '8000'
+bind_port = '80'
 
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind((bind_ip,bind_port))
